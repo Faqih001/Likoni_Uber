ad472e71c46da935c0cd35193e394c8a9eb8742e
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-13 22:27:42 +0300

    add comment to check for required fields in the request

--- app/(api)/ride/[id]+api.ts
+++ app/(api)/ride/[id]+api.ts
@@ -2,6 +2,8 @@ import { neon } from "@neondatabase/serverless";
 
 // Get the Request and Response types from the Neon Framework module
 export async function GET(request: Request, { id }: { id: string }) {
+
+  // Check if the required fields are present in the request
   if (!id)
     return Response.json({ error: "Missing required fields" }, { status: 400 });
 
