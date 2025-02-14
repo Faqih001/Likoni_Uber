cd802810fc542f700f2de2b61ef0feb2002f94c0
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-14 22:10:14 +0300

    add comment to describe the POST endpoint for creating a new user

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,5 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 
+// POST /api/user (create a new user) - creates a new user in the database with the given name, email, and clerkId
 export async function POST(request: Request) {
   try {
     const sql = neon(`${process.env.DATABASE_URL}`);
