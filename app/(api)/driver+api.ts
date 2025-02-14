90d8dc11bd66735d7c1167ae12a881633d800cef
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-14 22:09:32 +0300

    add comment to clarify request authorization check in the GET drivers endpoint

--- app/(api)/driver+api.ts
+++ app/(api)/driver+api.ts
@@ -2,8 +2,10 @@ import { neon } from "@neondatabase/serverless";
 
 // GET /api/driver (get all drivers) - returns all drivers in the database
 export async function GET(request: Request) {
+
   // Check if the request is authorized to access this endpoint
   try {
+    // Check if the request is authorized to access this endpoint
     const sql = neon(`${process.env.DATABASE_URL}`);
     const response = await sql`SELECT * FROM drivers`;
 
