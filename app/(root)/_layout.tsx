13cf1a5e3ee80a783a7580d2b055e84ead2e70ea
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-17 22:21:55 +0300

    add comment to clarify return value of the layout component for the navigation stack

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 
 // Layout component to define the navigation stack for the app
 const Layout = () => {
+  // Return the navigation stack for the app
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
