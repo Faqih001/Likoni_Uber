e4918444ae989b8f1163aee3be99b76bbb142b55
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-19 22:53:33 +0300

    add comment to clarify the Map component's purpose for directions to the destination

--- components/Map.tsx
+++ components/Map.tsx
@@ -13,9 +13,10 @@ import {
 import { useDriverStore, useLocationStore } from "@/store";
 import { Driver, MarkerData } from "@/types/type";
 
-// Map component to display the map with markers and directions
+// Map component to display the map with markers and directions to the destination
 const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
 
+// The map component uses the react-native-maps library to display the map
 const Map = () => {
   const {
     userLongitude,
